08b1dd11e71d7a3166ac5c8841a365d7edb5b4ae
JerryChang1997 <jerry@tulingc.com>
2023-04-13 16:37:02 +0800

    添加自动化操作

--- ads_power/auto_operate.py
+++ ads_power/auto_operate.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from core.information import search_one_data
+
 
 class AutoOperate(object):
 
@@ -15,20 +17,85 @@ class AutoOperate(object):
         self.driver.get("https://idp.hrblock.com/idp/profile/SAML2/Redirect/SSO?execution=e1s1")
         self.driver.find_element(
             By.XPATH,
-            "/html/body/main/div[1]/div[1]/div/div/div/div/section/div/div/div[1]/div/div/div[2]/a[1]").click()
-        self.driver.find_element(By.XPATH, '//*[@id="card1"]/div[2]/div/div[2]/div[2]/button').click()
-        self.driver.find_element(By.XPATH, '//*[@id="createID"]/a').click()
+            '//*[@id="main-nav"]/div[3]/div[2]/div/ul/li[3]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="mCSB_1_container"]/div/a[1]').click()
+        # self.driver.find_element(By.XPATH, '//*[@id="createID"]/a').click()
 
-    def register_email(self, email, username):
+    def register_email(self, email, username, password):
         self.driver.find_element(By.XPATH, '//*[@id="email"]/span/input').send_keys(email)
         self.driver.find_element(By.XPATH, '//*[@id="userName"]/span/input').send_keys(username)
         self.driver.find_element(By.XPATH, '//*[@id="showhideNew"]/button').click()
+        self.driver.find_element(By.XPATH, '//*[@id="password"]/span/input').send_keys(password)
+        self.driver.find_element(By.XPATH, '//*[@id="confirmPassword"]/span/input').send_keys(password)
+        # self.driver.find_element(By.XPATH, '//*[@id="createaccounttwo"]/hrb-checkbox[1]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="createaccounttwo"]/hrb-checkbox[2]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="submitButton"]/button').click()
+        self.driver.find_element(By.XPATH, '//*[@id="twoStepVerify"]/hrb-card-content/hrb-link/a').click()
+
+    def start_on_your_taxes(self):
+        self.driver.find_element(By.XPATH, '//*[@id="imbHero"]/button').click()
+        self.driver.find_element(By.XPATH, '//*[@id="imbPsBtnFour"]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="imbPsNext"]/button').click()
+
+        self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBlock4"]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="PageFooter1"]/div/div/div[1]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="StackPanelproformaimport"]/div/div[4]/div/fieldset/div/div[3]/div/div/div/div/label').click()
+        self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
+
+    def your_info(self, first_name, last_name, birthday, ssn, phone, address, zip_number):
+        # 个人信息
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxfirst_t"]').send_keys(first_name)
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxlast_t"]').send_keys(last_name)
+        date_of_birth, age = self.handle_date(birthday)
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxdob_t"]').send_keys(date_of_birth)
+        self.driver.find_element(By.XPATH, '//*[@id="list-option1"]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
+        # 社保号
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxtbTPSSN"]').send_keys(ssn)
+        self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
+        # 手机地址信息
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxTpDayPhone"]').send_keys(phone)
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxtbTPAddress"]').send_keys(address)
+        self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxtbTPZip"]').send_keys(zip_number)
+        self.driver.find_element(By.XPATH, '//*[@id="XRadioButtonrblOtherResidentStateY"]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
+        self.driver.find_element(By.XPATH, '//*[@id="PageFooter1"]/div/div/div[2]/a').click()
+        # were
+        self.driver.find_element(By.XPATH, '//*[@id="XRadioButtonrbmfjUSCitizenY"]').click()
+
+        # age
+        if age == "early":
+            self.driver.find_element(By.XPATH, '//*[@id="XRadioButtonrbSingleStatusY"]').click()
+            # 处理选择学生
+        else:
+            self.driver.find_element(By.XPATH, '//*[@id="XRadioButtonrbSingleStatusN"]').click()
+            # 处理不选择学生
+
+    def handle_date(self, birthday):
+        date_of_birth = birthday.strftime("%m/%d/%Y")
+        year = birthday.year
+        age = ""
+        if year >= 2000:
+            age = "early"
+        elif 1958 >= year >= 1940:
+            age = "old"
+        elif 1999 >= year >= 1959:
+            age = "common"
+        return date_of_birth, age
 
     def run(self, info_one):
         self.home_to_create_an_account()
-        self.register_email()
+        info_email = info_one["邮箱----密码"]
+        email, password = info_email.split("----")
+        self.register_email(email, info_one["账号"], info_one["密码"])
 
 
 if __name__ == '__main__':
-    operate = AutoOperate("127.0.0.1:50761")
-    operate.run()
+    # operate = AutoOperate("127.0.0.1:50761")
+    info_one, info_data = search_one_data()
+    print(info_one['生日'])
+    print(type(info_one['生日']))
+    # operate.run(info_one)
+    import datetime
+    now_date = datetime.datetime.now()
--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from core.information import search_one_data, change_value, search_index
 from ads_power import ads_power, s5_proxy
 from core.config import settings
 
-group_id = ads_power.AdsPower.search_group_id(settings.ADS_GROUP_NAME)
-user_proxy_config = s5_proxy.get_proxy()
-info_one, info_data = search_one_data()
-info_email = info_one["邮箱----密码"]
-email, password = info_email.split("----")
-print(email, password)
-# puuhxwbmts@hotmail.com----Mc7FxjqR
-user_id = ads_power.AdsPower.create_user(email, group_id, user_proxy_config)
-info_data = change_value(user_id, "user_id", search_index(info_email, info_data), info_data)
-result = ads_power.AdsPower.start_run_browser(user_id)
-print(result)
 
+def created_ads_power():
+    group_id = ads_power.AdsPower.search_group_id(settings.ADS_GROUP_NAME)
+    user_proxy_config = s5_proxy.get_proxy()
+    info_one, info_data = search_one_data()
+    info_email = info_one["邮箱----密码"]
+    email, password = info_email.split("----")
+    print(email, password)
+    # puuhxwbmts@hotmail.com----Mc7FxjqR
+    # 创建浏览器
+    user_id = ads_power.AdsPower.create_user(email, group_id, user_proxy_config)
+    info_data = change_value(user_id, "user_id", search_index(info_email, info_data), info_data)
+    result = ads_power.AdsPower.start_run_browser(user_id)
+    return result, info_data
+
+
+if __name__ == '__main__':
+    created_ads_power()
