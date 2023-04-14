594155add44f3533dc4b49b1d747a28852357fed
JerryChang1997 <jerry@tulingc.com>
2023-04-14 15:57:23 +0800

    添加自动化操作

--- ads_power/auto_operate.py
+++ ads_power/auto_operate.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from core.information import search_one_data
+from core.hotemail import get_mail
 
 
 class AutoOperate(object):
@@ -20,6 +21,27 @@ class AutoOperate(object):
             '//*[@id="main-nav"]/div[3]/div[2]/div/ul/li[3]/a').click()
         self.driver.find_element(By.XPATH, '//*[@id="mCSB_1_container"]/div/a[1]').click()
         # self.driver.find_element(By.XPATH, '//*[@id="createID"]/a').click()
+
+    def home_to_login(self, username, password, email_name, email_pwd):
+        self.driver.get("https://www.hrblock.com/")
+        self.driver.find_element(
+            By.XPATH,
+            '//*[@id="main-nav"]/div[3]/div[2]/div/ul/li[3]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
+        self.driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+        self.driver.find_element(By.XPATH, '//*[@id="mCSB_1_container"]/div/form/button').click()
+        self.verify_code(email_name, email_pwd)
+
+    def verify_code(self, email_name, email_pwd, depth=0):
+        code = get_mail(email_name, email_pwd)
+        if not code:
+            raise SystemExit
+        self.driver.find_element(By.XPATH, '//*[@id="siEmailSCode"]/span/input').clear()
+        self.driver.find_element(By.XPATH, '//*[@id="siEmailSCode"]/span/input').send_keys(code)
+        if self.is_exist("Error: Sorry, we don't recognize this security code. Please try again."):
+            if depth > 5:
+                raise SystemExit
+            self.verify_code(email_name, email_pwd, depth+1)
 
     def register_email(self, email, username, password):
         self.driver.find_element(By.XPATH, '//*[@id="email"]/span/input').send_keys(email)
@@ -40,7 +62,8 @@ class AutoOperate(object):
         self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
         self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBlock4"]/a').click()
         self.driver.find_element(By.XPATH, '//*[@id="PageFooter1"]/div/div/div[1]/a').click()
-        self.driver.find_element(By.XPATH, '//*[@id="StackPanelproformaimport"]/div/div[4]/div/fieldset/div/div[3]/div/div/div/div/label').click()
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="StackPanelproformaimport"]/div/div[4]/div/fieldset/div/div[3]/div/div/div/div/label').click()
         self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
 
     def your_info(self, first_name, last_name, birthday, ssn, phone, address, zip_number):
@@ -49,6 +72,7 @@ class AutoOperate(object):
         self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxlast_t"]').send_keys(last_name)
         date_of_birth, age = self.handle_date(birthday)
         self.driver.find_element(By.XPATH, '//*[@id="XFormatTextBoxdob_t"]').send_keys(date_of_birth)
+        self.driver.find_element(By.XPATH, '//*[@id="XListBoxxmaritalStatus-shdo"]').click()
         self.driver.find_element(By.XPATH, '//*[@id="list-option1"]').click()
         self.driver.find_element(By.XPATH, '//*[@id="btnNext"]').click()
         # 社保号
@@ -84,18 +108,29 @@ class AutoOperate(object):
             age = "common"
         return date_of_birth, age
 
+    def is_exist(self, str_to_find):
+        if str_to_find in self.driver.page_source:
+            return True
+        return False
+
     def run(self, info_one):
         self.home_to_create_an_account()
         info_email = info_one["邮箱----密码"]
         email, password = info_email.split("----")
-        self.register_email(email, info_one["账号"], info_one["密码"])
+        try:
+            self.register_email(email, info_one["账号"], info_one["密码"])
+        except Exception as e:
+            print("账号已存在，进入登录流程")
+            self.home_to_login(info_one["账号"], info_one["密码"], email, password)
+        self.start_on_your_taxes()
+        self.your_info(info_one["名"], info_one["姓"], info_one["生日"],
+                       info_one["社保号"], info_one["电话"], info_one["街道"], info_one["邮编"])
 
 
 if __name__ == '__main__':
-    # operate = AutoOperate("127.0.0.1:50761")
+    operate = AutoOperate("127.0.0.1:50761")
     info_one, info_data = search_one_data()
-    print(info_one['生日'])
-    print(type(info_one['生日']))
-    # operate.run(info_one)
-    import datetime
-    now_date = datetime.datetime.now()
+    try:
+        operate.run(info_one)
+    except:
+        operate.run(info_one)
--- core/hotemail.py
+++ core/hotemail.py
@@ -1,56 +1,31 @@
-# 记得要import
-# 用于邮件解码
-import pyzmail
-# 用于登录
-import imapclient
+import time
+
+import zmail, re
 
 
-def get_mail(username, password):
-    imapObj = imapclient.IMAPClient('outlook.office365.com', port=993)
-    imapObj.login(username, password)
-    select_info = imapObj.select_folder('Inbox', readonly=False)
-    # 打印当前收件箱邮件数
-    print('%d messages in INBOX' % select_info[b'EXISTS'])
-    # 搜索未读邮件
-    UIDS = imapObj.search('UNSEEN')
-    # 如果未读邮件大于1 执行以下操作
-    if len(UIDS) >= 1:
-        for UID in UIDS:
-            Rawmessages = imapObj.fetch(UID, [b'BODY[]'])
-            messages = pyzmail.PyzMessage.factory(Rawmessages[UID][b'BODY[]'])
-            # 获取邮件抬头
-            emailtitle = messages.get_subject()
-            # 打印邮件抬头
-            print(emailtitle)
-            # 获取邮件正文
-            emailmessage = messages.text_part.get_payload().decode(messages.text_part.charset)
-            # 打印邮件正文
-            print(emailmessage)
-            # 如果邮件内容包含 Test 则 返回HELLO
-            if 'Test' in emailmessage:
-                print('hello')
-                return 'hello'
-            # 否则的话就返回 error
-            else:
-                print('error')
-                return 'erro'
+def get_mail(username, password, depth=0):
 
-
-
-# Send mail
-# server.send_mail('yourfriend@example.com', {'subject': 'Hello!', 'content_text': 'By zmail.'})
-# # Or to a list of friends.
-# server.send_mail(['friend1@example.com', 'friend2@example.com'], {'subject': 'Hello!', 'content_text': 'By zmail.'})
-
-
-
-if __name__ == '__main__':
-    # puuhxwbmts@hotmail.com----Mc7FxjqR
-    # get_mail("puuhxwbmts@hotmail.com", "Mc7FxjqR")
-    import zmail
-
-    server = zmail.server("puuhxwbmts@hotmail.com", "Mc7FxjqR")
+    server = zmail.server(username, password)
 
     # Retrieve mail
     latest_mail = server.get_latest()
-    zmail.show(latest_mail)
+    mail_content = latest_mail['Content_html'][0]
+    # 使用正则表达式匹配验证码
+    code_pattern = re.compile(r'\b\d{6}\b')
+    match = code_pattern.search(mail_content)
+    # 提取验证码
+    if match:
+        code = match.group(0)
+        return code
+    else:
+        if depth < 10:
+            time.sleep(2)
+            get_mail(username, password, depth+1)
+        print("未检查到验证码，且超出重试次数")
+        return None
+
+
+if __name__ == '__main__':
+    # tpaapjj@hotmail.com----s69AP5BcGR
+    code = get_mail("tpaapjj@hotmail.com", "s69AP5BcGR")
+    print(code)
